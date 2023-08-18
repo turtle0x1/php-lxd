20d4f45ebc1c17f6953b0b526210f760df4c66dc
Dominik Müller <dominik@lacodix.de>
2023-08-18 09:54:18 +0200

    added endpoint-detection for exec-output

--- src/Endpoint/Instance/Logs.php
+++ src/Endpoint/Instance/Logs.php
@@ -3,6 +3,7 @@
 namespace Opensaucesystems\Lxd\Endpoint\Instance;
 
 use Opensaucesystems\Lxd\Endpoint\AbstractEndpoint;
+use Opensaucesystems\Lxd\Exception\InvalidEndpointException;
 
 class Logs extends AbstractEndpoint
 {
@@ -61,5 +62,21 @@ class Logs extends AbstractEndpoint
     public function remove($name, $log)
     {
         return $this->delete($this->getEndpoint().$name.'/logs/'.$log);
+    }
+
+    public function __get($endpoint)
+    {
+        $className =  basename(str_replace('\\', '/', get_class($this)));
+        $class = __NAMESPACE__.'\\'.$className.'\\'.ucfirst($endpoint);
+
+        if (class_exists($class)) {
+            $class = new $class($this->client);
+            $class->setEndpoint($this->getEndpoint());
+            return $class;
+        } else {
+            throw new InvalidEndpointException(
+                'Endpoint '.$class.', not implemented.'
+            );
+        }
     }
 }
